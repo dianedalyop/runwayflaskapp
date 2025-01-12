8609c68d854b0aa0f1f05feb5b2a0e82411a140f
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2025-01-12 14:08:19 +0000

    real time object detection in app implemented

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from pubnub.callbacks import SubscribeCallback
+from flask_cors import CORS
 app = Flask(__name__)
 
+CORS(app)
 # PubNub configuration
-pnconfig = PNConfiguration()
+"""pnconfig = PNConfiguration()
 pnconfig.subscribe_key = "sub-c-cf192832-cac4-4813-a037-6ba18e798dcd"
 pnconfig.publish_key = "pub-c-75f67faf-dd39-4f1c-9d61-c252d6b322e8"
 pnconfig.uuid = "flight-flask-app-uuid"
@@ -23,11 +25,10 @@ class MySubscribeCallback(SubscribeCallback):
 
 pubnub.add_listener(MySubscribeCallback())
 pubnub.subscribe().channels("flight-channel").execute()
-
+"""
 @app.route("/")
-def home():
-    return render_template("index.html", message=latest_message)
-
+def index():
+    return render_template('index.html')
 
 @app.route('/land')
 def land():
